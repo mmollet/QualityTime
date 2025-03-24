9ae39f6022fdc8fc34b0326f2061f0c8ae389e5c
MMollet <MMollet@NBMMWORK>
2025-03-24 08:37:43 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace QualityTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(47, 35);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.StopButton.Location = new System.Drawing.Point(47, 65);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.StartButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.StopButton;
+            this.ClientSize = new System.Drawing.Size(175, 128);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QualityTime";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace QualityTime
+{
+    public partial class Form1 : Form
+    {
+        private bool _isMoving = false;
+        private Thread _movementThread;
+        private Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "QualityTime"; // Set the title bar text
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (_isMoving) return;
+
+            _isMoving = true;
+            StartButton.Enabled = false;
+            StopButton.Enabled = true;
+
+            Console.WriteLine("QualityTime: Mouse movement started."); // Debug output
+
+            _movementThread = new Thread(MoveMouse);
+            _movementThread.IsBackground = true;
+            _movementThread.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            _isMoving = false;
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+
+            Console.WriteLine("QualityTime: Mouse movement stopped."); // Debug output
+
+            if (_movementThread != null && _movementThread.IsAlive)
+            {
+                _movementThread.Join(2000);
+                if (_movementThread.IsAlive)
+                {
+                    Console.WriteLine("QualityTime: Movement thread did not stop gracefully.");
+                }
+                _movementThread = null;
+            }
+        }
+
+        private void MoveMouse()
+        {
+            while (_isMoving)
+            {
+                Point currentPosition = Cursor.Position;
+                int xOffset = _random.Next(-10, 11); // Increased range
+                int yOffset = _random.Next(-10, 11); // Increased range
+                Point newPosition = new Point(currentPosition.X + xOffset, currentPosition.Y + yOffset);
+
+                try
+                {
+                    this.Invoke((MethodInvoker)delegate { Cursor.Position = newPosition; });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error setting Cursor.Position: {ex.Message}");
+                }
+
+                Console.WriteLine($"QualityTime: Mouse moved to X:{newPosition.X}, Y:{newPosition.Y}"); // Debug output
+                Thread.Sleep(60 * 1000);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopButton_Click(sender, e);
+        }
+    }
+}
